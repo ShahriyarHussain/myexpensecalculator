36f57c21b439fcb8610caa7c1f9a12b03bbdfc45
ShahriyarHussain <shossainshihab36@gmail.com>
2022-06-02 00:41:07 +0600

    Initial commit

---
+++ src/main/java/com/shahriyar/myexpensecalculator/MyexpensecalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.shahriyar.myexpensecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyexpensecalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyexpensecalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shahriyar/myexpensecalculator/MyexpensecalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shahriyar.myexpensecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyexpensecalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
